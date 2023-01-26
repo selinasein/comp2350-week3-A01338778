e645536782fc6e1061254c7830b6c7b03ebb5613
selinasein <neulmisscj@gmail.com>
2023-01-25 18:35:17 -0800

    edited

--- index.js
+++ index.js
@@ -1,69 +1,64 @@
-const mysql = require('mysql2/promise');
-const http = require('http');
+const mysql = require("mysql2/promise");
+const http = require("http");
 
 const port = process.env.PORT || 3000;
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfig = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+  host: "sql.freedb.tech",
+  user: "freedb_2350_main_sel",
+  password: "Qk28*ry9NTw*w8z",
+  database: "freedb_comp2350-A01338778",
+  multipleStatements: false,
 };
 
 var database = mysql.createPool(dbConfig);
 
 async function printMySQLVersion() {
-	let sqlQuery = `
+  let sqlQuery = `
 		SHOW VARIABLES LIKE 'version';
 	`;
-	
-	try {
-		const results = await database.query(sqlQuery);
-		console.log("Successfully connected to MySQL");
-		console.log(results[0]);
-		return true;
-	}
-	catch(err) {
-		console.log("Error getting version from MySQL");
-		return false;
-	}
+
+  try {
+    const results = await database.query(sqlQuery);
+    console.log("Successfully connected to MySQL");
+    console.log(results[0]);
+    return true;
+  } catch (err) {
+    console.log("Error getting version from MySQL");
+    return false;
+  }
 }
 
+http
+  .createServer(function (req, res) {
+    console.log("page hit");
+    const success = printMySQLVersion();
 
-http.createServer(function(req, res) {
-	console.log("page hit");
-	const success = printMySQLVersion();
-	
-	if (success) {
-		//Send an HTTP Status code of 200 for success!
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		if (is_qoddi) {
-			//write the HTML
-			res.end(`<!doctype html><html><head></head><body>
+    if (success) {
+      //Send an HTTP Status code of 200 for success!
+      res.writeHead(200, { "Content-Type": "text/html" });
+      if (is_qoddi) {
+        //write the HTML
+        res.end(`<!doctype html><html><head></head><body>
 			<div>Running on Qoddi</div>
 			<div>Connected to the database, check the Qoddi logs for the results.</div>
 			</body></html>`);
-		}
-		else {
-			//write the HTML
-			res.end(`<!doctype html><html><head></head><body>
+      } else {
+        //write the HTML
+        res.end(`<!doctype html><html><head></head><body>
 			<div>Running on localhost</div>
 			<div>Connected to the database, check the Qoddi logs for the results.</div>
 			</body></html>`);
-		}
-	}
-	else {
-		//Send an HTTP Status code of 500 for server error.
-		res.writeHead(500, {'Content-Type': 'text/html'});
-		//write the HTML
-		res.end(`<!doctype html><html><head></head><body>
+      }
+    } else {
+      //Send an HTTP Status code of 500 for server error.
+      res.writeHead(500, { "Content-Type": "text/html" });
+      //write the HTML
+      res.end(`<!doctype html><html><head></head><body>
 		<div>Database error, check the Qoddi logs for the details.</div>
 		</body></html>`);
-		console.log("Error connecting to mysql");
-	}
-}).listen(port);
-
-
-
+      console.log("Error connecting to mysql");
+    }
+  })
+  .listen(port);
